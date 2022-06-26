d2eed9e8747ee26a13c6ec3c36ad0c67ac0f4021
dmccomas <dmccomas63@gmail.com>
2022-06-26 15:28:28 -0700

    Beta bitValence version

--- src/pi_iolib_init.c
+++ src/pi_iolib_init.c
@@ -1,12 +1,26 @@
-/* 
-** Purpose: Generic entry point function for MIPEA library
+/*
+**  Copyright 2022 bitValence, Inc.
+**  All Rights Reserved.
 **
-** Notes:
-**   None
+**  This program is free software; you can modify and/or redistribute it
+**  under the terms of the GNU Affero General Public License
+**  as published by the Free Software Foundation; version 3 with
+**  attribution addendums as found in the LICENSE.txt
 **
-** License:
-**   Written by David McComas, licensed under the copyleft GNU
-**   General Public License (GPL). 
+**  This program is distributed in the hope that it will be useful,
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**  GNU Affero General Public License for more details.
+**
+**  Purpose:
+**    Generic entry point function for MIPEA library
+**
+**  Notes:
+**    1. See gpio_demo_app.h for details.
+**
+**  References:
+**    1. OpenSatKit Object-based Application Developer's Guide.
+**    2. cFS Application Developer's Guide.
 **
 */
 
@@ -27,7 +41,7 @@
 uint32 PI_IOLIB_Init(void)
 {
 
-   OS_printf("PI_IOLIB Initialized. Version 1.0.0\n");
+   OS_printf("PI_IOLIB Initialized. Version 0.9.0\n");
    
    return OS_SUCCESS;
 
